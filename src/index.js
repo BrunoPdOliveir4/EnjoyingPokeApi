c826ec24ffe9181821e1c1eae3bedc7872cfcd9a
Bruno Pedroso <bpedrosodroliveira10@gmail.com>
2024-06-24 20:23:28 -0300

    Started visual part with references of pokemon anime pokedex

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 8082
+const PORT = 8082;
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'templates/index.html'))
-})
+});
+
+
+
+
+//** JS Principal file getter */
 app.get("/scripts/principal", (req,res) =>{
     res.sendFile(path.join(__dirname, "scripts/principal.js"))
+});
+
+//** CSS principal file getter */
+app.get("/styles/principal", (req,res) => {
+    res.sendFile(path.join(__dirname, "styles/style.css"))
 })
+//** iMAGES GET ROUTES */
+app.get("/image/icon", (req, res) => {
+    res.sendFile(path.join(__dirname, "icons/pokeballIcon.png"))
+})
+
+app.get("/image/garden", (req,res) => {
+    res.sendFile(path.join(__dirname, "icons/garden.jpg"));
+})
+
 
 app.listen(PORT, ()=> {
     console.log(`servidor rodando na porta: ${
         PORT
-    }`)    
+    }`);    
 })
--- src/scripts/principal.js
+++ src/scripts/principal.js
@@ -1,20 +1,15 @@
+let isntStarted = true;
+
+
 async function getPokemon(poke){
     return fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`).then(data => data.json());
 }
 
-const inputs = document.getElementById('pokeName');
-const answer = document.getElementById('answer');
-const btnLook = document.getElementById('look');
+let inputs;
+let answer;
+let btnLook;
 
-inputs.addEventListener('input', (event) => {
-    answer.innerHTML = inputs.value;
-});
 
-btnLook.addEventListener('click', (event) => {
-    callPokemon(inputs.value)
-    
-    
-})
 
 async function callPokemon(poke){
     let pokemon;
@@ -37,8 +32,86 @@ async function callPokePicture(poke){
 
 function drawPoke(poke, pokepicture){
     console.log(pokepicture)
+    
     answer.innerHTML = `
     <h1>${poke.name}</h1>
-    <img src=${pokepicture.front_default} width=500 height=500></img>
     `
+}
+const mainContainer = document.getElementById("mainContainer");
+
+/*    Em desenvolvimento para o pós click.
+
+mainContainer.addEventListener("click", (event) => {
+    console.log(event)
+    drawFirstPage();
+})*/
+const drawFirstPage = () => {
+    if(isntStarted){
+        isntStarted = false;
+        mainContainer.innerHTML = `
+        <div class="poke">
+            <h1 id="answer"></h1>
+            <div>
+                <div class="foreground" id="pokeImg">
+
+                </div>
+                <img src="http://localhost:8082/image/garden" width="210px" height="210px" alt="Garden">
+            </div>
+            
+
+        </div>
+        <div class="inputs">
+            <input type="text" id="pokeName">
+            <button type="button" id="look">Look for Poké</button>
+            <div class="input-btns">
+                <button class="btnNumbers">
+                    1
+                </button>
+                <button class="btnNumbers">
+                    2
+                </button>
+                <button class="btnNumbers">
+                    3
+                </button>
+                <button class="btnNumbers">
+                    4
+                </button>
+                <button class="btnNumbers">
+                    5
+                </button>
+                <button class="btnNumbers">
+                    6
+                </button>
+                <button class="btnNumbers">
+                    7
+                </button>
+                <button class="btnNumbers">
+                    8
+                </button>
+                <button class="btnNumbers">
+                    9
+                </button>
+                <button class="btnNumbers">
+                    0
+                </button>
+            </div>
+        </div>
+        `
+        startEvents();
+    }
+}
+
+function startEvents(){
+    inputs = document.getElementById('pokeName');
+        answer = document.getElementById('answer');
+        btnLook = document.getElementById('look');
+        inputs.addEventListener('input', (event) => {
+            answer.innerHTML = inputs.value;
+        });
+        
+        btnLook.addEventListener('click', (event) => {
+            callPokemon(inputs.value)
+            
+            
+        })
 }
